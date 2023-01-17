41053e52fa3b176d2d2a26bb4b0aaa6ae61971c7
rodrigolustosa <rodrigo.lustosa@usp.br>
2023-01-17 16:55:12 -0300

    make urls to download

--- inmet_download_1.R
+++ inmet_download_1.R
@@ -12,7 +12,31 @@
 
 # packages
 library(tidyverse)
+library(stringr)
 
 # directory and file names
+dir_data <- "banco_de_dados"
+dir_data_input <- "raw"
+dir_data_output <- "raw"
+
+# data information
+
+
+# functions ---------------------------------------------------------------
+
+# Name         : Return Urls from INMET to download
+# Description  : Receive years as input and return zip files urls to download
+# Written by   : Rodrigo Lustosa
+# Writing date : 17 Jan 2023 16:50 (GMT -03)
+return_urls_inmet_download <- function(years) {
+  url_prefix <- "https://portal.inmet.gov.br/uploads/dadoshistoricos/"
+  url_posfix <- ".zip"
+  str_c(url_prefix,years,url_posfix)
+}
+
+
+# download data -----------------------------------------------------------
+
+return_urls_inmet_download(2020:2025)
 
 
