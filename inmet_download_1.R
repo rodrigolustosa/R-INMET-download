e3d4eccad38d7b9274ec86bed76e4828ec2e2625
rodrigolustosa <rodrigo.lustosa@usp.br>
2023-01-17 17:40:46 -0300

    download data

--- inmet_download_1.R
+++ inmet_download_1.R
@@ -13,13 +13,17 @@
 # packages
 library(tidyverse)
 library(stringr)
+library(lubridate)
+library(RCurl)
 
 # directory and file names
 dir_data <- "banco_de_dados"
 dir_data_input <- "raw"
-dir_data_output <- "raw"
+dir_data_output <- "output"
 
 # data information
+date_start <- ymd("2019-01-01")
+date_end   <- ymd("2022-01-01")
 
 
 # functions ---------------------------------------------------------------
@@ -31,12 +35,41 @@ dir_data_output <- "raw"
 return_urls_inmet_download <- function(years) {
   url_prefix <- "https://portal.inmet.gov.br/uploads/dadoshistoricos/"
   url_posfix <- ".zip"
-  str_c(url_prefix,years,url_posfix)
+  return(str_c(url_prefix,years,url_posfix))
 }
+
+# Name         : Download INMET files
+# Description  : download raw zip INMET files (by year) and save them at dir_path
+# Written by   : Rodrigo Lustosa
+# Writing date : 17 Jan 2023 17:18 (GMT -03)
+download_inmet_files <- function(years,dir_path) {
+  # basic info
+  n_files     <- length(years)
+  files_names <- str_c(years,".zip")
+  urls        <- return_urls_inmet_download(years)
+  path <- file.path(dir_path, files_names)
+  # download each file
+  for (i in 1:n_files) {
+    message(str_c("Downloading year ",years[i]))
+    # download if url exists and file was not downloaded yet
+    if (!file.exists(path[i]))
+      if(url.exists(urls[i]))
+        download.file(urls[i], destfile = path[i], method="curl")
+  }
+}
+
+
+# data information --------------------------------------------------------
+
+# years to download
+year_start <- year(date_start)
+year_end   <- year(date_end)
 
 
 # download data -----------------------------------------------------------
 
-return_urls_inmet_download(2020:2025)
+dir_input <- file.path(dir_data,dir_data_input)
+download_inmet_files(year_start:year_end,dir_input)
+
 
 
