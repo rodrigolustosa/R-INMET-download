e02b1739e27eed3a24b3dad1b81364b4a0311fbb
rodrigolustosa <rodrigo.lustosa@usp.br>
2023-01-19 16:38:54 -0300

    read one file

--- inmet_download_1.R
+++ inmet_download_1.R
@@ -13,16 +13,18 @@
 # packages
 library(tidyverse)
 library(stringr)
+library(stringi)
 library(lubridate)
 library(RCurl)
 
 # directory and file names
 dir_data <- "banco_de_dados"
-dir_data_input <- "raw"
+dir_data_input  <- "raw"
 dir_data_output <- "output"
+dir_data_temp   <- "temp"
 
 # data information
-date_start <- ymd("2019-01-01")
+date_start <- ymd("2018-01-01")
 date_end   <- ymd("2022-01-01")
 
 
@@ -61,12 +63,36 @@ download_inmet_files <- function(years,dir_path) {
   }
 }
 
+# Name         : Remove complex format
+# Description  : Remove all symbols that are not proper for a header or 
+# something related
+# Written by   : Rodrigo Lustosa
+# Writing date : 19 Jan 2023 15:43 (GMT -03)
+rm.complex.format <- function(string){
+  # remove accents and other complex symbols
+  new_string <- stri_trans_general(string, "Latin-ASCII")
+  # remove characters inside parenthesis
+  new_string <- str_remove_all(new_string,"\\(.*\\)")
+  # remove spaces after and before string
+  new_string <- str_trim(new_string)
+  # replace spaces by underline
+  new_string <- str_replace_all(new_string," ","_")
+  # change upper to lower case
+  new_string <- str_to_lower(new_string)
+  # remove all other characters that aren't letters, digits or underline
+  new_string <- str_remove_all(new_string,"[^a-zA-Z0-9_\\s]")
+  # replace repeated underlines by a single underline
+  new_string <- str_replace_all(new_string,"_+","_")
+  return(new_string)
+}
+
 
 # data information --------------------------------------------------------
 
 # directories
 dir_input  <- file.path(dir_data,dir_data_input)
 dir_output <- file.path(dir_data,dir_data_output)
+dir_temp   <- file.path(dir_data,dir_data_temp)
 
 # years to download
 year_start <- year(date_start)
@@ -78,6 +104,55 @@ year_end   <- year(date_end)
 download_inmet_files(year_start:year_end,dir_input)
 
 
-# extract files basic info ------------------------------------------------
+# read one file -----------------------------------------------------------
 
-# path <- file.path(dir)
+path <- file.path(dir_input, str_c(year_start,".zip"))
+allzipfiles <- unzip(path, list=TRUE)
+# extract data files names and address inside zip file
+if(a$Length[1] == 0){
+  zipfolder             <- allzipfiles$Name[1]
+  filenames_with_zipdir <- allzipfiles$Name[-1]
+  filenames             <- str_remove(filenames_with_zipdir,zipfolder)
+}else{
+  zipfolder <- ""
+  filenames_with_zipdir <- a$Name
+  filenames             <- a$Name
+}
+
+
+# unzip one file in temporary directory
+file_unziped <- unzip(path,filenames_with_zipdir[1],
+                      exdir = dir_temp,junkpaths = T)
+
+# read first lines, with basic info
+con <- file(file_unziped,encoding = "ISO-8859-15") 
+first_lines <- readLines(con,9)
+close(con)
+
+# extract raw station basic info and raw dataframe header
+basic_info <- first_lines[-9]
+csv_header <- first_lines[ 9]
+
+# tidy basic info
+basic_info <- str_split(basic_info,":;")
+n_basic_info <- length(basic_info)
+for (i in 1:n_basic_info) {
+  names(basic_info)[i] <- basic_info[[i]][1]
+  basic_info[[i]]      <- basic_info[[i]][2]
+}
+names(basic_info) <- rm.complex.format(names(basic_info))
+
+# tidy header
+csv_header <- str_split(csv_header,";")[[1]]
+csv_header <- rm.complex.format(csv_header)
+
+# read data
+dados <- read.csv2(file_unziped, header = F, skip = 9, na.strings = "-9999",
+                   fileEncoding = "ISO-8859-15") 
+
+# insert header
+names(dados) <- csv_header
+  
+# remove unzipped file
+file.remove(file_unziped)
+
