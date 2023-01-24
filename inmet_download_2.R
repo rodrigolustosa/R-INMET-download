6a6788f12ce93ddd56c33759228cff6304bbc180
rodrigolustosa <rodrigo.lustosa@usp.br>
2023-01-24 20:42:30 -0300

    download one year sample data

--- inmet_download_2.R
+++ inmet_download_2.R
@@ -9,79 +9,69 @@
 # initialization ----------------------------------------------------------
 
 # packages
-# library(tidyverse)
+library(tidyverse)
 library(RSelenium)
+library(stringr)
 
 # directory and file names
 
-
 # start docker
-# sudo docker run -d -p 4445:4444 selenium/standalone-firefox:4.0.0
-docker_id <- system(
-  "sudo -kS docker run -d -p 4445:4444 selenium/standalone-firefox:4.0.0",
-  input=rstudioapi::askForPassword("Enter your password: "), intern = TRUE
-)
+# sudo docker run -d -p 4445:4444 -v /WORKING/DIRECTORY:/home/seluser/Downloads:rw -d selenium/standalone-firefox:2.53.1
+cmd <- str_c("sudo -kS docker run -d -p 4445:4444 -v ", getwd(),
+             ":/home/seluser/Downloads:rw -d selenium/standalone-firefox:2.53.1")
+docker_id <- system(cmd, intern = TRUE,
+                    input=rstudioapi::askForPassword("Enter your password: "))
+
+# set docker download information
+fprof <- makeFirefoxProfile(
+  list(browser.download.dir = "/home/seluser/Downloads",
+       browser.download.folderList = 2L,
+       browser.download.manager.showWhenStarting = FALSE,
+       browser.helperApps.neverAsk.saveToDisk = "multipart/x-zip,application/zip,application/x-zip-compressed,application/x-compressed,application/msword,application/csv,text/csv,image/png ,image/jpeg, application/pdf, text/html,text/plain,  application/excel, application/vnd.ms-excel, application/x-excel, application/x-msexcel, application/octet-stream"))
 
 # start remote Driver
 remDr <- remoteDriver(
   remoteServerAddr = "localhost",
   port = 4445L,
-  browserName = "firefox"
+  browserName = "firefox",
+  extraCapabilities = fprof
 )
 remDr$open()
 remDr$getStatus()
 
 # open URL
-remDr$navigate("https://mapas.inmet.gov.br/#")
+remDr$navigate("https://tempo.inmet.gov.br/tabela/mapa/V0500/2023-01-01")
 # remDr$refresh()
 
 remDr$screenshot(display = TRUE)
 
-# set Window Size and refresh page
-remDr$setWindowSize(800, 800, winHand = "current")
+# find date boxes (start and end)
+el_datas <- remDr$findElements(using = "css", "[type = 'date']")
+# list of backspace keys equal to number of characters in a date string
+erase_date_keys        <- as.list(rep("backspace",10))
+names(erase_date_keys) <- rep("key",10)
+# erase past date values and fill new dates 
+el_datas[[1]]$sendKeysToElement(append(erase_date_keys, list("2022-01-01")))
+el_datas[[2]]$sendKeysToElement(append(erase_date_keys, list("2022-12-31")))
+
+remDr$screenshot(display = TRUE)
+
+# find button to generate new table and click on it
+el_gtabela <- remDr$findElement(using = "tag name", "button")
+el_gtabela$clickElement()
+# el_gtabela$getElementText()  # "Gerar Tabela"
+
+remDr$screenshot(display = TRUE)
+
+# find button to download new generated table and click on it
+el_bCSV <- remDr$findElement(using = "tag name", "a")
+el_bCSV$clickElement()
+# el_bCSV$getElementText()  # "Baixar CSV"
+
 remDr$refresh()
-
-remDr$screenshot(display = TRUE)
-
-# find search icon and click on it
-el_lupa <- remDr$findElement(using = "css", "[class = 'icone_submenu fas fa-search']")
-el_lupa$clickElement()
-# el_lupa$getElementLocation()
-# el_lupa$getElementSize()
-# remDr$mouseMoveToLocation(webElement = el_lupa)
-# remDr$mouseMoveToLocation(x = 78/2,y = 50/2 + 90)
-# remDr$click(0)
-
-remDr$screenshot(display = TRUE)
-
-# find search box and write station name (search is done automatically)
-el_busca <- remDr$findElement(using = "css", "[id = 'search']")
-el_busca$sendKeysToElement(list("[V0500] SUZANO - SETE CRUZES - SP"))
-
-remDr$screenshot(display = TRUE)
-
-# move over the station point and click it
-move <- c(800*0.5, (800-85)*0.5) # station point is centered in window
-remDr$mouseMoveToLocation(x = move[1], y = move[2])
-# remDr$mouseMoveToLocation(x = move[1] - 1, y = move[2])
-# remDr$mouseMoveToLocation(x =           1, y = 0)
-
-remDr$screenshot(display = TRUE)
-
-remDr$click(0)
-
-remDr$screenshot(display = TRUE)
-
-
-
-
-
-
 
 # close remote driver
 remDr$close()
-
-# remDr$screenshot(display = TRUE)
 
 # close docker
 # sudo docker stop $(sudo docker ps -q)
