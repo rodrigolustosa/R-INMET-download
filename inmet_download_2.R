6bec8e99b9de7750fd8d988f9fb80d9c9eaf5354
rodrigolustosa <rodrigo.lustosa@usp.br>
2023-01-24 14:59:21 -0300

    start new way to download data

---
+++ inmet_download_2.R
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------- #
+# Name         : INMET Download (2)
+# Description  : Download meteorological data from the Instituto Nacional de 
+# Meteorologia (IMNET) using the url https://mapas.inmet.gov.br/#
+# Written by   : Rodrigo Lustosa
+# Writing date : 19 Jan 2023 17:23 (GMT -03)
+# ---------------------------------------------------------------------------- #
+
+# initialization ----------------------------------------------------------
+
+# packages
+# library(tidyverse)
+library(RSelenium)
+
+# directory and file names
+
